7f7b5b288169c5a0d83e9fdb60c3ae6b3d77c9fd
Rene Rubalcava <rrubalcava@esri.com>
2022-12-02 14:22:58 -0800

    chore: day1 golang

---
+++ golang/day01.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	f, err := os.ReadFile("../data/day1.txt")
+	check(err)
+
+	lines := string(f)
+
+	scanner := bufio.NewScanner(strings.NewReader(lines))
+
+	cals := 0
+
+	totals := make([]int, 0)
+
+	for scanner.Scan() {
+		val := scanner.Text()
+		if val == "" {
+			cals = 0
+		} else {
+			cal, err := strconv.Atoi(val)
+			check(err)
+			cals = cals + cal
+			totals = append(totals, cals)
+		}
+	}
+
+	sort.Ints(totals)
+
+	fmt.Println(totals[len(totals)-1])
+	fmt.Println(totals[len(totals)-1] + totals[len(totals)-2] + totals[len(totals)-3])
+}
